1d4bc05eb9aeb046a4f49acdfbb477f728c7072a
fwcd <fwcdmail@gmail.com>
2019-01-18 22:08:43 +0100

    Added SUB/SUBI instructions

--- src/ts/model/parse/Opcodes.ts
+++ src/ts/model/parse/Opcodes.ts
@@ -1,8 +1,12 @@
 import { AddInstruction } from "../instructions/AddInstruction";
 import { Instruction } from "../instructions/Instruction";
 import { AddImmediateInstruction } from "../instructions/AddImmediateInstruction";
+import { SubInstruction } from "../instructions/SubInstruction";
+import { SubImmediateInstruction } from "../instructions/SubImmediateInstruction";
 
 const OPCODES: { [code: string]: { new(...args: number[]): Instruction; }; } = {
 	"ADD": AddInstruction,
-	"ADDI": AddImmediateInstruction
+	"ADDI": AddImmediateInstruction,
+	"SUB": SubInstruction,
+	"SUBI": SubImmediateInstruction
 };
