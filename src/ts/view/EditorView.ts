eae4e6e8087e71346bfc711f4ef199b5c09809b6
fwcd <fwcdmail@gmail.com>
2019-01-19 00:31:14 +0100

    Added DLX grammar

--- src/ts/view/EditorView.ts
+++ src/ts/view/EditorView.ts
@@ -1,10 +1,13 @@
 /// <reference path="../../../node_modules/monaco-editor/monaco.d.ts" />
 
+import { DLX_GRAMMAR } from "./grammar/DLXGrammar";
+
 export class EditorView {
 	private editor: monaco.editor.IStandaloneCodeEditor;
 	
 	public initialize(): void {
 		const editorWidget = document.getElementById("editor");
+		this.setupLanguageConfig();
 		this.editor = monaco.editor.create(editorWidget, {
 			language: "dlx-assembly",
 			minimap: {
@@ -13,8 +16,14 @@ export class EditorView {
 			scrollBeyondLastLine: false,
 			autoIndent: true,
 			renderIndentGuides: false,
-			wordBasedSuggestions: false
+			wordBasedSuggestions: false,
+			theme: "vs-dark"
 		});
+	}
+	
+	private setupLanguageConfig(): void {
+		monaco.languages.register({ id: "dlx-assembly" });
+		monaco.languages.setMonarchTokensProvider("dlx-assembly", DLX_GRAMMAR);
 	}
 	
 	public relayout(): void {
---
+++ src/ts/view/grammar/DLXGrammar.ts
@@ -0,0 +1,19 @@
+/// <reference path="../../../../node_modules/monaco-editor/monaco.d.ts" />
+
+import { OPCODES } from "../../model/instructions/Opcodes";
+
+export const DLX_GRAMMAR = <monaco.languages.IMonarchLanguage> {
+	keywords: Object.keys(OPCODES),
+	tokenizer: {
+		root: [
+			[/[a-zA-Z_$][\w$]*/, { cases: {
+				"@keywords": "keyword",
+				"@default": "identifier"
+			}}],
+			[/\d+/, "number"]
+		],
+		comment: [
+			[/\/\//, "comment"]
+		]
+	}
+};
