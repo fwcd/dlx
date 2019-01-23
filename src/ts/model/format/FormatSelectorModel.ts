8b0fe478de57dcdc85d018b2d353deb7100e72cd
fwcd <fwcdmail@gmail.com>
2019-01-23 08:57:17 +0100

    Added format listeners

--- src/ts/model/format/FormatSelectorModel.ts
+++ src/ts/model/format/FormatSelectorModel.ts
@@ -1,3 +1,26 @@
+import { DataFormat } from "./DataFormat";
+import { DecimalFormat } from "./DecimalFormat";
+import { ListenerList, Listener } from "../utils/ListenerList";
+
 export class FormatSelectorModel {
+	private format: DataFormat = new DecimalFormat();
+	private formatListeners = new ListenerList<DataFormat>();
 	
+	public getFormat(): DataFormat {
+		return this.format;
+	}
+	
+	public setFormat(format: DataFormat): void {
+		this.format = format;
+		this.formatListeners.fire(format);
+	}
+	
+	public addFormatListener(listener: Listener<DataFormat>): void {
+		this.formatListeners.add(listener);
+		listener(this.format);
+	}
+	
+	public removeFormatListener(listener: Listener<DataFormat>): void {
+		this.formatListeners.remove(listener);
+	}
 }
