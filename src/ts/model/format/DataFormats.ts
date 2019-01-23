54c921c68fea485516ad3dd1b645616bf42302e9
fwcd <fwcdmail@gmail.com>
2019-01-23 08:54:34 +0100

    Added ASCII to DataFormats

--- src/ts/model/format/DataFormats.ts
+++ src/ts/model/format/DataFormats.ts
@@ -2,10 +2,12 @@ import { HexFormat } from "./HexFormat";
 import { DecimalFormat } from "./DecimalFormat";
 import { BinaryFormat } from "./BinaryFormat";
 import { OctalFormat } from "./OctalFormat";
+import { AsciiFormat } from "./AsciiFormat";
 
-const DATA_FORMATS = {
+export const DATA_FORMATS = {
 	"Decimal": new DecimalFormat(),
 	"Hex": new HexFormat(),
 	"Binary": new BinaryFormat(),
-	"Octal": new OctalFormat()
+	"Octal": new OctalFormat(),
+	"ASCII": new AsciiFormat()
 };
