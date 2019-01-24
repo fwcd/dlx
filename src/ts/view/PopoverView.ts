7873fa22479ce6c2e6822e2b13c1ed4711f29813
fwcd <fwcdmail@gmail.com>
2019-01-24 13:15:16 +0100

    Implemented basic popover functionality

--- src/ts/view/PopoverView.ts
+++ src/ts/view/PopoverView.ts
@@ -2,11 +2,26 @@ export class PopoverView {
 	private element = document.getElementById("popover");
 	private content?: HTMLElement;
 	
+	public constructor() {
+		const closeButton = document.createElement("button");
+		closeButton.addEventListener("click", () => this.setVisible(false));
+		this.setVisible(false);
+	}
+	
 	public show(content: HTMLElement): void {
 		if (this.content) {
 			this.element.removeChild(this.content);
 		}
 		this.element.appendChild(content);
 		this.content = content;
+		this.setVisible(true);
+	}
+	
+	private setVisible(visible: boolean): void {
+		if (visible) {
+			this.element.style.display = "block";
+		} else {
+			this.element.style.display = "none";
+		}
 	}
 }
--- src/ts/view/SettingsView.ts
+++ src/ts/view/SettingsView.ts
@@ -1,3 +1,7 @@
 export class SettingsView {
 	private element = document.createElement("div");
+	
+	public constructor() {
+		this.element.innerText = "Settings"; // TODO
+	}
 }
