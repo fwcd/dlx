24a0bbe53613897d26be52962afa851cd3830394
fwcd <fwcdmail@gmail.com>
2019-01-24 20:04:51 +0100

    Removed 'SignedInts' setting

--- src/ts/model/SettingsModel.ts
+++ src/ts/model/SettingsModel.ts
@@ -5,21 +5,13 @@ import { ListenerList, Listener } from "./utils/ListenerList";
  */
 export class SettingsModel {
 	private highlightLines = true;
-	private signedInts = true;
 	private highlightListeners = new ListenerList<boolean>();
-	private signedIntsListeners = new ListenerList<boolean>();
 	
 	public getHighlightLines(): boolean { return this.highlightLines; }
-	
-	public useSignedInts(): boolean { return this.signedInts; }
 	
 	public setHighlightLines(highlightLines: boolean): void {
 		this.highlightLines = highlightLines;
 		this.highlightListeners.fire(highlightLines);
-	}
-	
-	public setUseSignedInts(signedInts: boolean): void {
-		this.signedInts = signedInts;
 	}
 	
 	public addHighlightListener(listener: Listener<boolean>, callerID?: number): void {
@@ -29,13 +21,5 @@ export class SettingsModel {
 	
 	public removeHighlightListener(listener: Listener<boolean>): void {
 		this.highlightListeners.remove(listener);
-	}
-	
-	public addSignedIntsListener(listener: Listener<boolean>, callerID?: number): void {
-		this.signedIntsListeners.add(listener, callerID);
-	}
-	
-	public removeSignedIntsListener(listener: Listener<boolean>): void {
-		this.signedIntsListeners.remove(listener);
 	}
 }
