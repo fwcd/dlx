4d9de565d6c978551b5d4639a4ac26457b056e16
fwcd <fwcdmail@gmail.com>
2019-01-25 11:38:17 +0100

    Configurable register count

--- src/ts/view/storage/RegistersView.ts
+++ src/ts/view/storage/RegistersView.ts
@@ -8,9 +8,18 @@ import { CALLER_ID_GENERATOR } from "../../model/utils/NumberIdGenerator";
 const REGISTERS_VIEW_CALLER_ID = CALLER_ID_GENERATOR.nextID();
 
 export class RegistersView {
+	private model: ProcessorStorage;
+	private formatModel: FormatSelectorModel;
+	private settings: SettingsModel;
+	
 	private element = document.getElementById("registers");
+	private cellContainer?: HTMLElement;
 	
 	public initialize(model: ProcessorStorage, formatModel: FormatSelectorModel, settings: SettingsModel): void {
+		this.model = model;
+		this.formatModel = formatModel;
+		this.settings = settings;
+		
 		// Create header
 		const header = document.createElement("div");
 		header.classList.add("storage-header");
@@ -28,29 +37,39 @@ export class RegistersView {
 		const formatView = new FormatSelectorView(formatModel);
 		header.appendChild(formatView.getElement());
 		
+		this.updateCellViews();
+		model.addRegisterCountListener(() => this.updateCellViews());
+		
 		this.element.appendChild(header);
+		this.element.appendChild(this.cellContainer);
+	}
+	
+	private updateCellViews(): void {
+		if (this.cellContainer) {
+			this.model.removeRegisterListenersBy(REGISTERS_VIEW_CALLER_ID);
+			this.element.removeChild(this.cellContainer);
+		}
 		
-		// Create registers
-		const registerCount = model.getRegisterCount();
-		const cellContainer = document.createElement("div");
+		const registerCount = this.model.getRegisterCount();
 		
-		cellContainer.classList.add("storage-cell-container");
+		this.cellContainer = document.createElement("div");
+		this.cellContainer.classList.add("storage-cell-container");
 		
 		for (let i = 0; i < registerCount; i++) {
 			const cell = new StorageCellView({
-				getter: () => model.getRegister(i),
-				setter: value => model.setRegister(i, value, REGISTERS_VIEW_CALLER_ID),
-				formatModel: formatModel,
-				settings: settings,
+				getter: () => this.model.getRegister(i),
+				setter: value => this.model.setRegister(i, value, REGISTERS_VIEW_CALLER_ID),
+				formatModel: this.formatModel,
+				settings: this.settings,
 				name: "R" + i
 			});
 			if (i == 0) {
 				cell.setChangeable(false);
 			}
-			model.addRegisterListener(i, () => cell.update(), REGISTERS_VIEW_CALLER_ID);
-			cellContainer.appendChild(cell.getElement());
+			this.model.addRegisterListener(i, () => cell.update(), REGISTERS_VIEW_CALLER_ID);
+			this.cellContainer.appendChild(cell.getElement());
 		}
 		
-		this.element.appendChild(cellContainer);
+		this.element.appendChild(this.cellContainer);
 	}
 }
