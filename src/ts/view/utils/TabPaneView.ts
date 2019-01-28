7863e45e9b08d512f97d12797a4f1a6bbca27468
fwcd <fwcdmail@gmail.com>
2019-01-29 00:32:28 +0100

    Implemented automatic tab discovery in TabPaneView

--- src/ts/view/utils/TabPaneView.ts
+++ src/ts/view/utils/TabPaneView.ts
@@ -1,10 +1,27 @@
 import { View } from "./View";
 
+/**
+ * A container panel for labelled tabs.
+ */
 export class TabPaneView implements View {
 	private element: HTMLElement;
 	
 	public constructor(element: HTMLElement) {
 		this.element = element;
+		this.findExistingTabs();
+	}
+	
+	public findExistingTabs(): void {
+		const found = this.element.getElementsByClassName("tab");
+		for (let i = 0; i < found.length; i++) {
+			const tabElement = found.item(i) as HTMLElement;
+			const name = tabElement.dataset.tabName;
+			this.addTab(name, tabElement);
+		}
+	}
+	
+	public addTab(name: string, tabElement: HTMLElement): void {
+		// TODO
 	}
 	
 	public getElement(): HTMLElement {
