fa9ed78abb586b473ffe65717b1a9e3e8128bf36
fwcd <fwcdmail@gmail.com>
2019-02-11 23:08:44 +0100

    Tab selection mechanism

--- src/ts/view/utils/TabPaneView.ts
+++ src/ts/view/utils/TabPaneView.ts
@@ -1,11 +1,20 @@
 import { View } from "./View";
 
+interface Tab {
+	button: HTMLElement;
+	content: HTMLElement;
+}
+
+const SELECTED_CLASS = "tabbutton-selected";
+const UNSELECTED_CLASS = "tabbutton-unselected";
+
 /**
  * A container panel for labelled tabs.
  */
 export class TabPaneView implements View {
 	private paneElement: HTMLElement;
 	private barElement: HTMLElement;
+	private selectedTab?: Tab;
 	
 	public constructor(barElement: HTMLElement, paneElement: HTMLElement) {
 		this.barElement = barElement;
@@ -16,20 +25,43 @@ export class TabPaneView implements View {
 	public addExistingTabs(): void {
 		const found = this.paneElement.getElementsByClassName("tabcontent");
 		for (let i = 0; i < found.length; i++) {
-			const tabElement = found.item(i) as HTMLElement;
-			const name = tabElement.dataset.tabName;
-			this.addTab(name, tabElement);
+			const tabContent = found.item(i) as HTMLElement;
+			const name = tabContent.dataset.tabName;
+			this.addTab(name, tabContent);
 		}
 	}
 	
-	public addTab(name: string, tabElement: HTMLElement): void {
+	public addTab(name: string, tabContent: HTMLElement): void {
+		// Configure button
 		const tabButton = document.createElement("button");
-		tabButton.classList.add("tabbutton", "tabbutton-unselected");
+		tabButton.classList.add("tabbutton", UNSELECTED_CLASS);
 		tabButton.innerText = name;
-		tabButton.addEventListener("click", () => {
-			tabButton.classList.replace("tabbutton-unselected", "tabbutton-selected");
-		});
+		
+		// Setup tab compound and listeners
+		const tab = {
+			button: tabButton,
+			content: tabContent
+		};
+		tabButton.addEventListener("click", () => this.select(tab));
+		
+		if (!this.selectedTab) {
+			this.select(tab);
+		}
+		
 		this.barElement.appendChild(tabButton);
+	}
+	
+	private select(tab: Tab): void {
+		// Hide previous tab if present
+		if (this.selectedTab) {
+			this.selectedTab.button.classList.replace(SELECTED_CLASS, UNSELECTED_CLASS);
+			this.selectedTab.content.style.display = "none";
+		}
+		
+		// Prepare and assign new tab
+		tab.button.classList.replace(UNSELECTED_CLASS, SELECTED_CLASS);
+		tab.content.style.display = "block";
+		this.selectedTab = tab;
 	}
 	
 	public getElement(): HTMLElement {
